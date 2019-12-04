8b0b9c5225fe3c0cab6156da8b51fa05241c90f9
johanser92 <40702293+johanser92@users.noreply.github.com>
2019-12-04 00:49:01 -0400

    Update Peliculas
    prueba a ver ahi

--- Practica6MvCSQL/Models/Peliculas.cs
+++ Practica6MvCSQL/Models/Peliculas.cs
@@ -7,12 +7,12 @@ namespace Practica6MvCSQL.Models
 {
     public class Peliculas
     {
-        public int Codigo { get; set; }
+       // public int Codigo { get; set; } Borra esta parte
         public string Titulo { get; set; }
         public string Director { get; set; }
          public string AutorPrincipal { get; set; }
         public int NumAutores { get; set; }
-         public double Duracion { get; set; }
+         public Double Duracion { get; set; }//Aqui es Double
         public int Estreno { get; set; }
     }
 }
